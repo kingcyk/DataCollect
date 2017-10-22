ebb476f25cfdd708f408f6dfe9533eef79639b09
Chen Yukai <kingcyk@kingcyk.com>
2017-10-22 22:42:55 +0800

    Initial Commit

---
+++ DataCollect/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DataCollect
+//
+//  Created by kingcyk on 22/10/2017.
+//  Copyright © 2017 kingcyk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
